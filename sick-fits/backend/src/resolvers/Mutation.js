9e7a96a36309825792c155a443ecb2658d6f717e
Renan <renandf@gmail.com>
2019-01-03 21:47:14 +1100

    Module #5 - classes 24 to 28

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const Mutations = {
 	async createItem(parent, args, ctx, info) {
 		// TODO: Check if they are logged in
@@ -37,6 +40,56 @@ const Mutations = {
 		// TODO
 		// 3. delete it
 		return ctx.db.mutation.deleteItem({ where }, info);
+	},
+	async signup(parent, args, ctx, info) {
+		args.email = args.email.toLowerCase();
+		// hash the user password
+		const password = await bcrypt.hash(args.password, 10);
+		// create the user in the db
+		const user = await ctx.db.mutation.createUser(
+			{
+				data: {
+					...args,
+					password,
+					permissions: { set: ["USER"] }
+				}
+			},
+			info
+		);
+		// create the JWT token for the user
+		const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+		// set the jwt as a cookie on the response
+		ctx.response.cookie("token", token, {
+			httpOnly: true,
+			maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
+		});
+		// return the user to the browser
+		return user;
+	},
+	async signin(parent, { email, password }, ctx, info) {
+		// 1. check if there is a user with that email
+		const user = await ctx.db.query.user({ where: { email } });
+		if (!user) {
+			throw new Error(`No such user found for email ${email}`);
+		}
+		// 2. check if their password is correct
+		const valid = await bcrypt.compare(password, user.password);
+		if (!valid) {
+			throw new Error("Invalid Password!");
+		}
+		// 3. generate the JWT token
+		const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+		// 4. set the cookie with the token
+		ctx.response.cookie("token", token, {
+			httpOnly: true,
+			maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
+		});
+		// 5. return the user
+		return user;
+	},
+	signout(parent, args, ctx, info) {
+		ctx.response.clearCookie("token");
+		return { message: "Goodbye!" };
 	}
 };
 
